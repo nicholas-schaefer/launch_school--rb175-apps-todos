fa31e230f0949be35f65ecc0164b2cf1f4e5107e
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-06-21 09:13:18 -0400

    completed 5 viewing all todo lists

--- todo.rb
+++ todo.rb
@@ -2,6 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+# helpers do
+#   def in_list_items()
+#   end
+# end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = [
+    {name: "Lunch Groceries", todos:[]},
+    {name: "Dinner Groceries", todos:[]}
+  ]
+  erb :lists, layout: :layout
 end
