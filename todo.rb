b5fd34d6bf9284a6809410b3bdf14e55bd566628
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-06-21 17:25:32 -0400

    completed 11 refactoring validations

--- todo.rb
+++ todo.rb
@@ -26,9 +26,38 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid. Return nil if the name is valid
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters"
+  elsif session[:lists].any?{ |list| list[:name] == name }
+    "List name must be unique"
+  end
+end
+
 # Create a new list
 post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  session[:success] = "The list has been created."
-  redirect "/lists"
+    list_name = params[:list_name].strip
+
+    error = error_for_list_name(list_name)
+    if error
+      session[:error] = error
+      erb :new_list, layout: :layout
+    else
+      session[:lists] << { name: list_name, todos: [] }
+      session[:success] = "The list has been created."
+      redirect "/lists"
+    end
+
+    # if !(1..100).cover? list_name.size
+    #   session[:error] = "List name must be between 1 and 100 characters"
+    #   erb :new_list, layout: :layout
+    # elsif session[:lists].any?{ |list| list[:name] == list_name }
+    #   session[:error] = "List name must be unique"
+    #   erb :new_list, layout: :layout
+    # else
+    #   session[:lists] << { name: list_name, todos: [] }
+    #   session[:success] = "The list has been created."
+    #   redirect "/lists"
+    # end
 end
