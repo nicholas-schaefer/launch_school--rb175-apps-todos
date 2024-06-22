ee147abe02fb829f6fb2300dcc171d9831807fb8
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-06-22 11:30:59 -0400

    progress made 14 displaying a single todo list

--- todo.rb
+++ todo.rb
@@ -26,6 +26,34 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return a boolean true value if a_string is also an integer
+def is_i?(a_string)
+  !!(a_string =~ /\A[-+]?[0-9]+\z/)
+end
+
+# Return an error message if the list index is invalid. Return nil if the name is valid
+def error_for_list_index(index)
+  if (!is_i?(index)) || (!(0...@lists.size).cover? index.to_i)
+    "list not found"
+  end
+end
+
+# View single lists
+get "/lists/:list_id" do
+
+  @lists = session[:lists]
+  list_id = params[:list_id]
+
+  error = error_for_list_index(list_id)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list = @lists[list_id.to_i]
+    erb :list, layout: :layout
+  end
+end
+
 # Return an error message if the name is invalid. Return nil if the name is valid
 def error_for_list_name(name)
   if !(1..100).cover? name.size
@@ -48,16 +76,4 @@ post "/lists" do
       session[:success] = "The list has been created."
       redirect "/lists"
     end
-
-    # if !(1..100).cover? list_name.size
-    #   session[:error] = "List name must be between 1 and 100 characters"
-    #   erb :new_list, layout: :layout
-    # elsif session[:lists].any?{ |list| list[:name] == list_name }
-    #   session[:error] = "List name must be unique"
-    #   erb :new_list, layout: :layout
-    # else
-    #   session[:lists] << { name: list_name, todos: [] }
-    #   session[:success] = "The list has been created."
-    #   redirect "/lists"
-    # end
 end
